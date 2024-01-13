ef5223c16d93afc21245671b7643086dfa6c8253
siddanth-6365 <siddanth6365@gmail.com>
2024-01-13 13:08:21 +0530

    added nextra and intro

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <h3>Leetcode-Stats-RESTful-Api</h3>,
+    project: {
+      link: 'https://github.com/siddanth-6365/leetcode-Stats-api'
+    }
+    // ... other theme options
+  }
