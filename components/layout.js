29828c8e1af7937549d6414cd54e7e762cb4b343
siddanth-6365 <siddanth6365@gmail.com>
2024-01-14 03:13:30 +0530

    added few changes

---
+++ components/layout.js
@@ -0,0 +1,13 @@
+// components/Layout.js
+import Head from 'next/head';
+
+const Layout = ({  title }) => {
+  return (
+    <div>
+    piqnepi pw tpw tp iwetpiw tpi wetiwe tpw etpwi tw 
+    {title}
+    </div>
+  );
+};
+
+export default Layout;
